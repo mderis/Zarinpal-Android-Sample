b3178d9f490d91cec03b9a08e12e0605fb3bb859
MosleM <moslem.deris@gmail.com>
2016-12-20 17:36:47 +0330

    Initial commit - Empty project

---
+++ app/src/main/java/ir/moslem_deris/apps/zarinpal_android_sample/MainActivity.java
@@ -0,0 +1,13 @@
+package ir.moslem_deris.apps.zarinpal_android_sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
