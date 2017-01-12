ddc123f6393c7a73411b21798d7d9e3f8029c101
MosleM <moslem.deris@gmail.com>
2017-01-12 13:32:58 +0330

    Added some helpful comments.

--- app/src/main/java/ir/moslem_deris/apps/zarinpal_android_sample/MainActivity.java
+++ app/src/main/java/ir/moslem_deris/apps/zarinpal_android_sample/MainActivity.java
@@ -26,24 +26,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onPaymentButtonClicked(View view){
+        //  Show loading message
         textView.setText("در حال انتقال به صفحه پرداخت\nلطفا صبر کنید ...");
+        //  Start payment process
         pay();
     }
 
     private void pay(){
+        /* Prepare a Payment object with custom data.
+            The standard form we recommend is you create a constant value including your merchantID, and for
+            anything you want to sell create a unique payment object (instead of editing your current object times and times)
+            and pass the variable holding merchantID, this helps you edit your merchantID safe and easy anytime you wanted.
+        */
         Payment payment = new PaymentBuilder()
                 .setMerchantID("4ff2f25a-82c8-45eb-b540-4c5b5ee8aeb5")  //  This is an example, put your own merchantID here.
                 .setAmount(1000)                                        //  In Toman
                 .setDescription("پرداخت تست پلاگین اندروید")
-                .setEmail("moslem.deris@gmail.com")                     //  This field is custom.
-                .setMobile("09123456789")                               //  This field is custom.
+                .setEmail("moslem.deris@gmail.com")                     //  This field is not necessary.
+                .setMobile("09123456789")                               //  This field is not necessary.
                 .create();
 
+        /* Call pay method and pass your considered payment object.
+            Don't call a payment before the last one is finished, if you do the first payment will be destroyed
+            and you may even not receive any callbacks (onSuccess or onFailure)
+        */
         ZarinPal.pay(this, payment, new OnPaymentListener() {
             @Override
             public void onSuccess(String refID) {
                 Toast.makeText(MainActivity.this, refID, Toast.LENGTH_LONG).show();
-                Log.wtf("TAG", "Main Activity: " + refID);
+                Log.wtf("TAG", "::ZarinPal::  RefId: " + refID);
                 textView.setText("پرداخت با موفقیت انجام شد\nکد پیگیری: " + refID);
             }
 
@@ -56,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
                     case NOT_ENOUGH_DATA: errorMessage = "اطلاعات پرداخت کافی نیست";    break;
                     case UNKNOWN:         errorMessage = "خطای ناشناخته";              break;
                 }
-                Log.wtf("TAG", "Main Activity: " + "ERROR: " + errorMessage);
+                Log.wtf("TAG", "::ZarinPal::  ERROR: " + errorMessage);
                 textView.setText("خطا!!!" + "\n" + errorMessage);
             }
         });
